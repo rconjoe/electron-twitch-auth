03015c894e2af54c31917b62f34693113361f724
rconjoe <root@trog.codes>
2025-03-05 18:51:30 -0800

    extract header

---
+++ src/ui/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+
+  return (
+    <div className="flex items-center justify-center mb-4 mt-4">
+      <img src="../images/2025-03-03_20-48.png" className='w-8 h-8' />
+      <h1 className="text-5xl text-center px-5">darfmike</h1>
+      <img src="../images/2025-03-03_20-48.png" className='w-8 h-8' />
+    </div>
+  )
+}
