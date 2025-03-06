85182c658d2bd88319a0c290e8cab6b397840e4b
rconjoe <root@trog.codes>
2025-03-05 18:51:35 -0800

    moving things around

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,29 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
+import { ThemeProvider } from './ui/components/theme-provider';
+import Header from './ui/components/header';
+import TwitchConnect from './ui/blocks/twitch-connect';
 
 export default function App() {
 
-  return <><h1>dickhead</h1>
-    <p>This app is using node v{window.versions.node()}, electron v{window.versions.electron()}, chrome v{window.versions.chrome()}, and react v{React.version}</p>
-  </>
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Header />
+      <div className="flex items-center justify-center">
+        <TwitchConnect />
+      </div>
+    </ThemeProvider>
+  )
+
 }
 
-const root = createRoot(document.getElementById('app'));
+// eslint-disable-next-line
+const root = createRoot(document.getElementById('app')!);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+
